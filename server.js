196d0e0d0a9912bf2313a4736d4c911bc9d98ea1
Edward Seim <sirseim@gmail.com>
2017-02-16 21:09:05 -0800

    Setup the basic server

--- server.js
+++ server.js
@@ -1,1 +1,80 @@
 require('dotenv').config();
+const Hapi = require('hapi');
+const Path = require('path');
+const Vision = require('vision');
+const Inert = require('inert');
+const NunjucksHapi = require('nunjucks-hapi');
+const Good = require('good');
+
+const postgresPool = require(Path.join(__dirname, 'plugins/postgres-pool.js'));
+
+const setup = {
+  host: process.env.NODE_ENV === 'production' ? '0.0.0.0' : 'localhost',
+  port: process.env.PORT || '8080',
+};
+
+const Api = require(Path.join(__dirname, 'api'));
+// console.log(Api);
+const viewRoutes = require(Path.join(__dirname, 'routes/view_routes.js'));
+
+const BasicServer = new Hapi.Server({
+  connections: {
+    routes: {
+      files: {
+        relativeTo: Path.join(__dirname, 'static'),
+      },
+    },
+  },
+});
+BasicServer.connection({
+  host: setup.host,
+  port: setup.port,
+});
+BasicServer.register(postgresPool);
+BasicServer.register(Api, {
+  routes: {
+    prefix: '/api',
+  },
+});
+
+BasicServer.register(Inert);
+BasicServer.register(Vision, () => {
+  BasicServer.views({
+    engines: {
+      html: NunjucksHapi,
+    },
+    path: Path.join(__dirname, 'templates'),
+  });
+  BasicServer.route(viewRoutes);
+});
+
+BasicServer.register({
+  register: Good,
+  options: {
+    ops: {
+      interval: 1000,
+    },
+    reporters: {
+      console: [{
+        module: 'good-squeeze',
+        name: 'Squeeze',
+        args: [{
+          log: '*',
+          response: '*',
+        }],
+      }, {
+        module: 'good-console',
+      }, 'stdout'],
+    },
+  },
+}, (err) => {
+  if (err) {
+    BasicServer.log(['error', 'good'], err);
+  }
+});
+
+BasicServer.start(() => {
+  BasicServer.log(['info', 'BasicServer'], `Server started on ${setup.host}:${setup.port}`);
+});
+
+module.exports = BasicServer;
