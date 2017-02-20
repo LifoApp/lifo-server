c9976ce1b81520bbcc9acba95130128dfd930763
Edward Seim <sirseim@gmail.com>
2017-02-20 07:59:44 -0800

    ESLint appeasement commit
    Postgres isn't ready for primetime

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const Inert = require('inert');
 const NunjucksHapi = require('nunjucks-hapi');
 const Good = require('good');
 
-const postgresPool = require('./plugins/postgres-pool.js');
+// const postgresPool = require('./plugins/postgres-pool.js');
 
 const Api = require('./api');
 const Views = require('./views');
