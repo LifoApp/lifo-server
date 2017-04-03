9e59bfa10c4340ba11c967bc9ae58ec47c8f1a91
Edward Seim <sirseim@gmail.com>
2017-04-03 13:07:23 -0700

    Take data and upload it
    It should actually work

--- api/Collection/Controller.js
+++ api/Collection/Controller.js
@@ -1,3 +1,5 @@
+const Promise = require('bluebird');
+
 const server = require('./../../server.js');
 
 const paths = [
@@ -5,12 +7,28 @@ const paths = [
     method: 'POST',
     path: '/address',
     handler: (req, res) => {
+      const DEFAULT_ID = {
+        name: '00:00:00:00:00:00',
+        timestamp: '2017-01-01T00:00:00.000Z',
+        latitude: 33.9698,
+        longitude: -118.4169,
+      };
       // console.log(server.sequelize.models.);
       server.log(['info', 'testing'], req.payload);
-      res({
-        statusCode: 200,
-        message: 'Welcome to the Lifo API',
-      }).code(200);
+
+      const time = req.payload.time;
+      Promise.map(req.payload.clients, (client) => {
+        const data = DEFAULT_ID;
+        data.name = client || data.name;
+        data.timestamp = time || data.timestamp;
+        return server.sequelize.models.Identifier.create(data);
+      }).then((result) => {
+        res({
+          statusCode: 201,
+          message: 'Successfully added addresses',
+          result,
+        });
+      });
     },
   },
   {
