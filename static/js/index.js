c73806edf331463c371d4eee388cfc23376efcb4
Edward Seim <sirseim@gmail.com>
2017-05-03 12:07:56 -0700

    Basic counter up and running

--- static/js/index.js
+++ static/js/index.js
@@ -1,8 +1,24 @@
 (($) => {
-  $.ajax({
-    dataType: 'json',
-    url: '/api/locations/1/count',
-  }).then((data) => {
-    console.log(data);
-  });
+  const updateLiveCount = () => {
+    const end = moment();
+    const start = moment(end).subtract(5, 'minutes');
+    const url = `/api/locations/1/count?start=${start.utc().toISOString()}&end=${end.utc().toISOString()}&slice=5`;
+    $.ajax({
+      dataType: 'json',
+      url,
+    }).then((data) => {
+      $('#live-count').text(data.result[0].count);
+      console.log(data);
+    }, (error) => {
+      console.log(error);
+    });
+  };
+
+  const updateInterval = 1000 * 30;
+  let updateTimer$ = 0;
+  const updateTimer = () => {
+    updateLiveCount();
+    updateTimer$ = setTimeout(updateTimer, updateInterval);
+  };
+  updateTimer();
 })($);
